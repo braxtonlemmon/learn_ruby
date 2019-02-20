55e69cf56add243689f75b0f3a45c1dfd043b5d8
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-02-20 13:38:40 +0100

    Complete exercise 02. Write new tests for exercise 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
-#write your code here
+def add(a,b)
+    a + b
+end
+
+def subtract(a, b)
+    a - b
+end
+
+def sum(numbers)
+    total = 0
+    numbers.each { |number| total += number }
+    total
+end
+
+def multiply(*numbers)
+    total = 1
+    numbers.each { |number| total *= number }
+    total
+end
+
+def power(a, b)
+    a ** b
+end
+
+def factorial(number)
+    if number < 0
+        "Number cannot be negative!"
+    elsif number == 0
+        1
+    else
+        total = 1
+        while number > 0 do
+            total *= number
+            number -= 1
+        end
+        total
+    end
+end
