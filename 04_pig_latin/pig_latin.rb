fe246f23f5230836044bf4a129bdb7091d63cdf2
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-02-20 14:36:37 +0100

    Complete exercise 04. Write additional test for exercise 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,20 @@
-#write your code here
+def translate(phrase)
+    phrase = phrase.split(' ').map do |word|
+        if word[0].downcase.match(/[aeiou]/)
+            new_word = word + 'ay'
+        else
+            new_word = word.split('')
+            word.each_char do |char|
+                if char.downcase.match(/[^aeio]/)
+                    new_word.shift
+                    new_word << char
+                else
+                    break
+                end
+            end
+            new_word = new_word.join('') + 'ay'
+        end
+        (word.capitalize == word) ? new_word.capitalize : new_word
+    end
+    phrase.join(' ')
+end
