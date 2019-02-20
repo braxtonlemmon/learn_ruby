3025b2c622553c0cf36869a52d4d48988ab44298
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-02-20 14:52:22 +0100

    Complete exercise 06. Implement extra tests for exercise 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,19 @@
 class Timer
-  #write your code here
+  attr_accessor :hours, :minutes, :seconds
+
+  def seconds (seconds=0)
+    @seconds = seconds
+  end
+
+  def time_string
+    @hours = @seconds / 3600
+    @minutes = @seconds % 3600 / 60
+    @seconds = @seconds % 3600 % 60
+    "#{padded(@hours)}:#{padded(@minutes)}:#{padded(@seconds)}"
+  end
+
+  def padded(number)
+    "%02d" % number
+  end
+
 end
