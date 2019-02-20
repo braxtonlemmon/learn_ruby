0b32dba113a6e595ccb9b8b293c6822b8bad6301
Braxton Lemmon <braxtonlemmon@gmail.com>
2019-02-20 13:58:47 +0100

    Complete exercise 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,30 @@
-#write your code here
+def echo(phrase)
+    phrase
+end
+
+def shout(phrase)
+    phrase.upcase
+end
+
+def repeat(phrase, repeats = 2)
+    response = []
+    repeats.times { response << phrase }
+    response.join(' ')
+end
+
+def start_of_word(word, letters)
+    word.split('').slice(0, letters).join('')
+end
+
+def first_word(phrase)
+    phrase.split(' ')[0]
+end
+
+def titleize(title)
+    bad = ['a', 'an', 'the', 'and', 'but', 'or', 'for', 'nor',
+           'on', 'at', 'to', 'from', 'by', 'over']
+    title = title.split(' ').map { |word| bad.include?(word) ? word : word.capitalize }
+    title[0] = title[0].capitalize
+    title.join(' ')
+end
+
